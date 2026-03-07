87bb5fa2be8d506089365d8f01751177083fccfe
Sammy0202 <126312030+Sammy0202@users.noreply.github.com>
2026-03-07 12:26:24 +0530

    Implement typing effect for text display

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Portfolio Loaded");
+const text = "Backend Developer | AI Engineer | Machine Learning";
+
+let i = 0;
+
+function typing(){
+
+if(i < text.length){
+
+document.querySelector(".typing").innerHTML += text.charAt(i);
+
+i++;
+
+setTimeout(typing,50);
+
+}
+
+}
+
+typing();
