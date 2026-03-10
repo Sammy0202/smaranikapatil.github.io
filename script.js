c7f134b4a97a523a240bb9478537a9defeeaa279
Sammy0202 <126312030+Sammy0202@users.noreply.github.com>
2026-03-10 09:54:51 +0530

    Refactor particle initialization and remove cert functions
    Refactor particlesJS initialization and remove openCert and closeCert functions.

--- script.js
+++ script.js
@@ -19,54 +19,24 @@ setTimeout(typing,50);
 typing();
 
 
-particlesJS("particles-js",{
+particlesJS("particles-js", {
+
 particles:{
 number:{value:80},
 size:{value:3},
 move:{speed:2},
 line_linked:{enable:true}
+
 }
+
 });
 
 
 ScrollReveal().reveal('section',{
+
 delay:200,
 distance:'40px',
 duration:1000,
 origin:'bottom'
+
 });
-
-
-function openCert(type){
-
-let html="";
-
-if(type==="python"){
-html=`<img src="images/python1.jpg">`;
-}
-
-if(type==="ml"){
-html=`
-<img src="images/ml1.jpg">
-<img src="images/ml2.jpg">
-`;
-}
-
-if(type==="web"){
-html=`<img src="images/web1.jpg">`;
-}
-
-if(type==="security"){
-html=`<img src="images/security1.jpg">`;
-}
-
-document.getElementById("certContainer").innerHTML=html;
-
-document.getElementById("certModal").style.display="block";
-
-}
-
-
-function closeCert(){
-document.getElementById("certModal").style.display="none";
-}
